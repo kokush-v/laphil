adb939762cb2553a1612953343a317fe684c62b4
kokush <witalik.logoyda@gmail.com>
2023-07-28 23:19:21 +0300

    test refactoring

--- test/ticket.test.ts
+++ test/ticket.test.ts
@@ -3,26 +3,16 @@ import app from "../src/app";
 import { expect } from "chai";
 import { Ticket } from "../src/types";
 
-class ApiTest {
-	packageId: string;
+describe("Test /tickets endpoint", () => {
+	const packageId = "1196";
 
-	constructor(packageId: string) {
-		this.packageId = packageId;
-	}
+	it("should respond with array of avaible tickets", async () => {
+		const response = await request(app).get("/tickets").query({ package_id: packageId });
 
-	ticketTest() {
-		describe("Test /tickets endpoint", () => {
-			it("should respond with array of avaible tickets", async () => {
-				const response = await request(app).get("/tickets").query({ package_id: this.packageId });
-
-				expect(response.status).to.equal(200);
-				expect(response.body).to.be.an("array");
-				response.body.forEach((ticket: any) => {
-					expect(ticket).to.be.an("object");
-				});
-			}).timeout(5000);
+		expect(response.status).to.equal(200);
+		expect(response.body).to.be.an("array");
+		response.body.forEach((ticket: Ticket) => {
+			expect(ticket).to.be.an("object");
 		});
-	}
-}
-
-new ApiTest("1196").ticketTest();
+	}).timeout(10000);
+});
